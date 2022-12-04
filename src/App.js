fcb2bd2dd8f97670ec245db1c22b31cd7f41181c
rajatRS <rajatshukla2012.rs@gmail.com>
2022-12-04 23:41:27 +0530

    Final App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Container, Grid, Image, Item, Button } from "semantic-ui-react";
+import "./App.css";
+import Header from "./Header";
+import imageCompression from "browser-image-compression";
+
+
 
 function App() {
+  const [origImage, setOrigImage] = useState("");
+  const [origImageFile, setOrigImageFile] = useState("");
+  const [compressedImage, setCompressedImage] = useState("");
+  const [fileName, setFileName] = useState("");
+
+  const handle = (e) => {
+    const imageFile = e.target.files[0];
+    setOrigImage(imageFile);
+    setOrigImageFile(URL.createObjectURL(imageFile));
+    setFileName(imageFile.name);
+  };
+
+  const handleCompressImage = (e) => {
+    e.preventDefault();
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 500,
+      useWebWorker: true,
+    };
+
+    if (options.maxSizeMB >= origImage / 1024) {
+      alert("Image is too small, cant be compressed");
+      return 0;
+    }
+
+    let output;
+    imageCompression(origImage, options).then((x) => {
+      output = x;
+      const downloadLink = URL.createObjectURL(output);
+      setCompressedImage(downloadLink);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <h1></h1> */}
+      <Header />
+      <div className = "home">
+          <div className = "home_first">
+            <Item>
+              {origImageFile ? (
+                <Image src={origImageFile}></Image>
+              ) : (
+                <Image src="http://navparivartan.in/wp-content/uploads/2018/11/placeholder.png"></Image>
+              )}
+            </Item>
+          </div>
+
+          <div className = "home_second">
+            <input
+              type="file"
+              accept="image/*"
+              className="mt-2 btn btn-dark w-75 home_button1"
+              onChange={(e) => handle(e)}
+            />
+            <h1></h1>
+            {origImageFile && (
+              <Button
+              primary
+              className = "home_button"
+                onClick={(e) => {
+                  handleCompressImage(e);
+                }}
+              >
+                {" "}
+                Compress Image
+              </Button>
+            )}
+            <h1></h1>
+            {compressedImage && (
+              <Button  className = "home_button">
+                <a href={compressedImage} download={fileName}>
+                  {" "}
+                  Download Image
+                </a>
+              </Button>
+            )}
+          </div>
+
+          <div className = "home_third">
+            <Item>
+              {compressedImage ? (
+                <Image src={compressedImage}></Image>
+              ) : (
+                <Image src="http://navparivartan.in/wp-content/uploads/2018/11/placeholder.png"></Image>
+              )}
+            </Item>
+          </div>
+      </div>
     </div>
+
   );
+
 }
 
+
+
 export default App;
+
+
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css";
+function Header() {
+  return (
+      <div className = "header">
+          {/* <h1>Compression App</h1> */}
+          Image Compressing Web App
+      </div>
+  )
+}
+
+export default Header;
